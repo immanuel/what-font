1af7a38b385272dad9ec30a39f12478389a022c7
Immanuel Thomas <immanuel278@gmail.com>
2017-02-26 22:04:49 -0800

    Added Chart.js bar chart to display char count

--- popup.js
+++ popup.js
@@ -1,17 +1,49 @@
 // Send message to content script to get font usage in current tab
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {msg: "getFonts"}, function(response) {
-        // TODO: Get the count and display it
-        var div = document.getElementById('count');
-        var content;
+        var div = document.getElementById('char-count-container');
+
+        // Get the count and display it in a bar chart
+        var ctx = document.getElementById("char-count-chart");
+
+        var labels = [];
+        var data = [];
 
         for (var font in response) {
             if (response.hasOwnProperty(font)) {
-                console.log(font + " used by " + response[font] + " character(s)");
-                content = document.createElement('p');  
-                content.innerHTML = font + " used by " + response[font] + " character(s)";
-                div.appendChild(content);
+                labels.push(font);
+                data.push(response[font]);
             }
         }
+
+        var data = {
+            labels: labels,
+            datasets: [
+                {
+                    borderWidth: 1,
+                    data: data
+                }
+            ]
+        };
+        var options = {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    display: false
+                }],
+                yAxes: [{
+                    gridLines: {
+                        display: false
+                    }
+                }]
+            }
+        };
+        var charCountChart = new Chart(ctx, {
+            type: 'horizontalBar',
+            data: data,
+            options: options
+        });
     });
 });
