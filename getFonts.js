e2e322c6c930a92892e9943c2e5caafc087d6eb7
Immanuel Thomas <immanuel278@gmail.com>
2017-03-11 00:42:46 -0800

    Added new chart to track font usage by font size

--- getFonts.js
+++ getFonts.js
@@ -1,4 +1,5 @@
 var fontUsage = {};
+var fontSizeUsage = {};
 
 // Calculate the number of characters using each font, under elem and aggregate
 // in fontUsage
@@ -14,9 +15,21 @@ function getFonts(elem) {
     elemText = elemText.replace(/\s/gm, "");
     
     // Add the length to the elem's font-family's running count
+    //      and to the fontSizeUsage under the respective font-size
     // TODO: Handle case where ',' is present in the font name within '"'
     if (elemText.length > 0) {
         var elemFont = elem.css("font-family").split(",")[0].replace(/"/g, "").toLowerCase();
+        var elemSize = elem.css("font-size").match('^[0-9.]*')[0]; 
+
+        if (elemSize in fontSizeUsage) {
+            fontSizeUsage[elemSize][elemFont] = elemText.length + 
+                ((elemFont in fontSizeUsage[elemSize])? fontSizeUsage[elemSize][elemFont] : 0);
+        }
+        else {
+            fontSizeUsage[elemSize] = {};
+            fontSizeUsage[elemSize][elemFont] = elemText.length;
+        }
+        
         fontUsage[elemFont] = elemText.length + 
             ((elemFont in fontUsage)? fontUsage[elemFont] : 0);
     }
@@ -33,11 +46,11 @@ chrome.runtime.onMessage.addListener(
         if (request.msg == "getFonts") {
 
             // Initialize usage, incase the message is sent multiple times
-            fontUsage = {}
+            fontUsage = {};
 
             getFonts($("body"));
 
             // send parse results back to popup
-            sendResponse(fontUsage);
+            sendResponse({fontUsage: fontUsage, fontSizeUsage: fontSizeUsage});
         }
     });
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 // Set the chart font
-// Chart.defaults.global.defaultFontFamily = "'Montserrat', sans-serif";
+Chart.defaults.global.defaultFontFamily = "'Lato', sans-serif";
 var defaultColors = [
     '#96ccff',
     '#9eebcf',
@@ -15,23 +15,27 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {msg: "getFonts"}, function(response) {
         var div = document.getElementById('char-count-container');
 
-        // Get the count and display it in a bar chart
-        var ctx = document.getElementById("char-count-chart");
-
         var labels = [];
         var data = [];
-        var bgcolors = []
+        var size_data = [];
+        var bgcolors = [];
         var index = 0;
-
-        for (var font in response) {
-            if (response.hasOwnProperty(font)) {
+                
+        // TODO: calculate font total from the fontSizeUsage
+        for (var font in response.fontUsage) {
+            if (response.fontUsage.hasOwnProperty(font)) {
                 labels.push(font);
-                data.push(response[font]);
+                data.push(response.fontUsage[font]);
                 bgcolors.push(defaultColors[index % numColors]);
+
+                size_data.push({label: font, backgroundColor: defaultColors[index % numColors], data: []});
+
                 index++;
             }
         }
 
+        // Display bar chart for simple font usage
+        var ctx = document.getElementById("char-count-chart");
         var data = {
             labels: labels,
             datasets: [
@@ -43,24 +47,76 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             ]
         };
         var options = {
-            legend: {
-                display: false
-            },
+            legend: { display: false },
             scales: {
-                xAxes: [{
-                    display: false
-                }],
-                yAxes: [{
-                    gridLines: {
-                        display: false
-                    }
-                }]
+                xAxes: [{ display: false }],
+                yAxes: [{ gridLines: { display: false } }]
             }
         };
         var charCountChart = new Chart(ctx, {
             type: 'horizontalBar',
             data: data,
             options: options
+        });
+
+        // Display line chart for distribution of size and font
+        var size_ctx = document.getElementById("char-size-chart");
+
+        /*
+        size_data = [
+            {label:'lato', bgclor: blue, data: []}, 
+            {label:'lato', bgclor: blue, data: []}, 
+            {label:'lato', bgclor: blue
+                data: [{
+                    x: -5,
+                    y: 0
+                }, {
+                    x: 0,
+                    y: 15
+                }, {
+                    x: 20,
+                    y: 5
+                }]
+
+            }
+        ]
+        */
+
+        var fontSizeCount;
+        var size_data_length = size_data.length;
+        console.log(response.fontSizeUsage);
+        for (var fontSize in response.fontSizeUsage) {
+            if (response.fontSizeUsage.hasOwnProperty(fontSize)) {
+                fontSizeCount = response.fontSizeUsage[fontSize];
+
+                // TODO: convert to % distribution
+                for (var i = 0; i < size_data_length; i++) {
+                    size_data[i].data.push({
+                        x: fontSize, 
+                        y: ((size_data[i].label in fontSizeCount) ? fontSizeCount[size_data[i].label] : 0)
+                    });
+                }
+            }
+        }
+
+        var size_options = {
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
+                }], 
+                yAxes: [{
+                    stacked: 'true'
+                }]
+            }
+        };
+
+        console.log(size_data);
+
+        var charSizeChart = new Chart(size_ctx, {
+            type: 'line', 
+            data: {datasets: size_data},
+            options: size_options
         });
     });
 });
